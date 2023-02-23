a9d016e4a864369890e3d50dde57812d049e736d
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-02-23 09:26:34 +0300

    Different pages for different actions that will happen in the application

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,19 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/products')
+def products():
+    return render_template('products.html')
+
+@main.route('/stocks')
+def stocks():
+    return render_template('stocks.html')
+
+@main.route('/sales')
+def sales():
+    return render_template('sales.html')
+
+@main.route('/point_of_sale')
+def point_of_sale():
+    return render_template('point_of_sale.html')
