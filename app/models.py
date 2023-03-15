ef9b091452b1b4a0e1e5159202ef4f7a857ae11d
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-03-15 16:54:40 +0300

    account confirmation

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask import current_app
+import jwt
+import datetime
 
 class Role(db.Model):
     __tablename__ = "roles"
@@ -17,6 +20,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @property
@@ -29,6 +33,33 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+
+    def generate_confirmation_token(self, expiration=3600):
+        confirmation_token = jwt.encode({
+            "confirm": self.id,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=expiration)
+        },
+        current_app.config['SECRET_KEY'],
+        algorithm="HS256"
+        )
+        return confirmation_token
+    
+    def confirm(self, token):
+        try:
+            data = jwt.decode(
+                token,
+                current_app.config['SECRET_KEY'],
+                leeway=datetime.timedelta(seconds=10),
+                algorithms=["HS256"]
+            )
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
 
 @login_manager.user_loader
