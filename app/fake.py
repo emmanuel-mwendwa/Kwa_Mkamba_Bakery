9c71b32cda860af8cf06d9b0eb9f000de95c716a
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-04-23 10:38:36 +0300

    updated template

--- app/fake.py
+++ app/fake.py
@@ -1,7 +1,7 @@
 from random import randint
 from faker import Faker
 from . import db
-from .models import Product, Production_Run
+from .models import Product, ProductionRun
 
 def products(num_products=10, num_production_runs=50):
     fake = Faker()
@@ -18,11 +18,12 @@ def products(num_products=10, num_production_runs=50):
     # Generate fake production runs
     product_ids = [p.id for p in Product.query.all()]
     for i in range(num_production_runs):
-        pr = Production_Run(
+        pr = ProductionRun(
             product_id = fake.random_element(elements=product_ids),
             quantity=randint(1, 100),
             flour_kneaded=randint(1, 100),
-            date_created=fake.date_time_between(start_date='-1y', end_date='now')
+            oil_used=randint(1, 4),
+            created_at=fake.date_time_between(start_date='-1y', end_date='now')
         )
         db.session.add(pr)
 
