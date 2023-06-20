ccdf1d4a43312fab456691c627871d4b5779ec4f
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-06-20 22:21:41 +0300

    CRUD operations for customers

--- app/sales/views.py
+++ app/sales/views.py
@@ -3,3 +3,19 @@ from . import sales
 @sales.route("/")
 def sales():
     return "Sales Page"
+
+@sales.route("/new_customer")
+def new_customer():
+    return "Add Customer"
+
+@sales.route("/view_customers")
+def view_customers():
+    return "View Customers"
+
+@sales.route("/view_customer/<int:id>")
+def view_customer(id):
+    return "View Customer"
+
+@sales.route("/edit_customer")
+def edit_customer():
+    return "Edit Customer"
