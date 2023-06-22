7ca7880fb38fa87772fe2e991fad4045456f0a32
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-06-22 23:26:41 +0300

    Add and Edit Customer functions

---
+++ app/sales/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SubmitField
+from wtforms.validators import DataRequired, Regexp, Email, Length
+from wtforms import ValidationError
+from ..models import User
+
+class AddNewCustomerForm(FlaskForm):
+    cust_name = StringField("Customer Name: ", validators=[DataRequired(), Length(0, 64), Regexp('^[A-Za-z ]*$',0, 'Names can only contain letters and spaces')])
+    cust_email = StringField("Customer Email: ", validators=[DataRequired(), Length(1, 64), Email()])
+    cust_phone_no = StringField('Phone Number: ', validators=[Length(0, 13), Regexp('^[0-9+]*$', 0, 'Phonw number can only contain numbers')])
+    mpesa_agent_name = StringField("Mpesa Name: ", validators=[])
+    submit = SubmitField("Submit")
+
+
+    def validate_email(self, field):
+        if field.data != self.user.email and\
+        User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_phone_no(self, field):
+        if field.data:
+            if field.data.startswith('0'):
+                field.data = field.data[1:]
+                field.data = '+254' + field.data
+            elif not field.data.startswith('+'):
+                field.data = '+' + field.data
--- app/sales/views.py
+++ app/sales/views.py
@@ -1,4 +1,10 @@
 from . import sales
+from flask import redirect, render_template, url_for, flash, request
+from flask_login import login_required
+from .forms import AddNewCustomerForm
+from .. import db
+from ..models import Customer
+from ..decorators import admin_required, permission_required
 
 @sales.route("/")
 def sales_page():
@@ -6,6 +12,17 @@ def sales_page():
 
 @sales.route("/new_customer")
 def new_customer():
+    customer_form = AddNewCustomerForm()
+    if customer_form.validate_on_submit():
+        new_customer = Customer(
+           cust_name = customer_form.cust_name.data,
+           cust_email = customer_form.cust_email.data,
+           cust_phone_no = customer_form.cust_phone_no.data,
+            cust_mpesa_agent_name = customer_form.mpesa_agent_name.data
+        )
+        db.session.add(new_customer)
+        db.session.commit()
+        flash("Customer added successfully", category="success")
     return "Add Customer"
 
 @sales.route("/view_customers")
@@ -16,6 +33,22 @@ def view_customers():
 def view_customer(id):
     return "View Customer"
 
-@sales.route("/edit_customer")
-def edit_customer():
+@sales.route("/edit_customer/<int:id>")
+def edit_customer(id):
+    customer = Customer.query.get_or_404(id)
+    customer_form = AddNewCustomerForm()
+    if customer_form.validate_on_submit():
+        customer.cust_name = customer_form.cust_name.data
+        customer.cust_email = customer_form.cust_email.data
+        customer.cust_phone_no = customer_form.cust_phone_no.data
+        customer.cust_mpesa_agent_name = customer_form.mpesa_agent_name.data
+
+        db.session.add(customer)
+        db.session.commit()
+        flash("Customer added successfully", category="success")
+        return "Edit Customer"
+    customer_form.cust_name.data = customer.cust_name
+    customer_form.cust_email.data = customer.cust_email
+    customer_form.cust_phone_no.data = customer.cust_phone_no
+    customer_form.mpesa_agent_name.data = customer.cust_mpesa_agent_name
     return "Edit Customer"
